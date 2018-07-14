4ae5e656f815614bc057df0b4a9f4c02a1691fb0
Pascal DANEK <pdanek@kds.com>
2018-07-14 18:15:06 +0200

    Removed draft of rpc api tests

--- src/test/scala/myproject/api/methods/NewUserSpecs.scala
+++
@@ -1,8 +0,0 @@
-package myproject.api.methods
-
-import org.scalatest.DoNotDiscover
-
-@DoNotDiscover
-class NewUserSpecs extends RPCMethodSpecs("save_user") {
-  pending
-}
--- src/test/scala/test/TestSuites.scala
+++
@@ -1,8 +0,0 @@
-package test
-
-import myproject.api.methods.{LoginPasswordSpecs, WelcomeSpecs}
-import org.scalatest.Suites
-
-class TestSuites extends Suites(
-  new LoginPasswordSpecs,
-  new WelcomeSpecs)
