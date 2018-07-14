f26be9281ddf416c94fa80b8312000e09ac3ed15
Pascal DANEK <pdanek@kds.com>
2018-07-14 18:42:56 +0200

    Refined the way we reset the database Removed default admin user directly written in db

--- src/main/scala/myproject/database/DB.scala
+++ src/main/scala/myproject/database/DB.scala
@@ -1,9 +1,5 @@
 package myproject.database
 
-import java.util.UUID
-
-import myproject.common.security.BCrypt
-import myproject.iam.Users.User
 import myproject.iam.dao.{TokenDAO, UserDAO}
 import slick.jdbc.JdbcProfile
 
@@ -13,11 +9,7 @@ object DB extends JdbcProfile with UserDAO with TokenDAO {
 
   lazy val db = Database.forConfig("database")
 
-  def reset = db.run(DBIO.seq(
-    users.schema.drop.asTry,
-    tokens.schema.drop.asTry,
-    tokens.schema.create,
-    users.schema.create,
-    users ++= Seq(
-      User(UUID.fromString("e498dd4e-2758-4e01-80f6-392f4f43606b"), "admin", BCrypt.hashPassword("Kondor_123")))))
+  val schema = users.schema ++ tokens.schema
+
+  def reset = db.run(DBIO.seq(schema.drop, schema.create))
 }
