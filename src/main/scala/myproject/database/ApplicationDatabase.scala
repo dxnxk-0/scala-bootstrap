f15c622ac8fc9863fb63ae545535df5349b798e5
Pascal DANEK <pdanek@kds.com>
2018-09-12 22:15:02 +0200

    Minor fix

--- src/main/scala/myproject/database/ApplicationDatabase.scala
+++ src/main/scala/myproject/database/ApplicationDatabase.scala
@@ -6,9 +6,12 @@ import myproject.iam.Groups.GroupDAO
 import myproject.iam.Tokens.TokenDAO
 import myproject.iam.Users.UserDAO
 import myproject.iam.dao.{SlickChannelDAO, SlickGroupDAO, SlickTokenDAO, SlickUserDAO}
-import slick.jdbc.JdbcProfile
 
-trait ApplicationDatabase extends DatabaseInterface with JdbcProfile with SlickUserDAO with SlickTokenDAO with SlickGroupDAO with SlickChannelDAO
+trait ApplicationDatabase extends DatabaseInterface
+  with SlickUserDAO
+  with SlickTokenDAO
+  with SlickGroupDAO
+  with SlickChannelDAO
 
 object ApplicationDatabase {
   val productionDatabaseImpl = Class.forName(Config.database.implClassName).newInstance.asInstanceOf[DatabaseInterface with ChannelDAO with GroupDAO with UserDAO with TokenDAO]
