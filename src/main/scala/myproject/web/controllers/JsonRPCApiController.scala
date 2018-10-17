ab0c61cca7443154920b301b68f8c77d08de0bac
Pascal DANEK <pdanek@kds.com>
2018-10-17 13:00:16 +0200

    CORS support (final)

--- src/main/scala/myproject/web/controllers/JsonRPCApiController.scala
+++ src/main/scala/myproject/web/controllers/JsonRPCApiController.scala
@@ -44,17 +44,15 @@ object JsonRPCApiController extends Controller {
         handleRejections(RejectionHandler.default) { // As soon as the URL match we don't want to evaluate other URLs
           optionalHeaderValueByName("Remote-Address") { ip =>
             authenticateOAuth2Async(realm = "rpc-api", authenticator = WebAuth.jwtAuthenticator) { user =>
-              respondWithHeaders(`Access-Control-Allow-Origin`(HttpOriginRange.*)) {
-                post {
-                  entity(as[RPCRequest]) { req =>
-                    onComplete(processRpcRequest(user, req, ip)) { res =>
-                      completeOpRpc(res)
-                    }
-                  } ~
-                  entity(as[Seq[RPCRequest]]) { batch =>
-                    onComplete(processBatchRpcRequest(user, batch, ip)) { res =>
-                      completeOpRpcBatch(res)
-                    }
+              post {
+                entity(as[RPCRequest]) { req =>
+                  onComplete(processRpcRequest(user, req, ip)) { res =>
+                    completeOpRpc(res)
+                  }
+                } ~
+                entity(as[Seq[RPCRequest]]) { batch =>
+                  onComplete(processBatchRpcRequest(user, batch, ip)) { res =>
+                    completeOpRpcBatch(res)
                   }
                 }
               }
