3c59674a3a575880e7ee78c9923cd903d928dcbd
Pascal DANEK <pdanek@kds.com>
2018-12-05 12:33:13 +0100

    Fixed wrong rpc method security

--- src/main/scala/myproject/api/functions/SendMagicLink.scala
+++ src/main/scala/myproject/api/functions/SendMagicLink.scala
@@ -10,6 +10,7 @@ import myproject.iam.Users.{CRUD, UserDAO}
 class SendMagicLink(implicit db: UserDAO with TokenDAO, notifier: Notifier) extends ApiFunction {
   override val name = "send_magic_link"
   override val doc = ApiSummaryDoc("send to an email address a magic link in order to login to the platform", "nothing is returned")
+  override val secured: Boolean = false
 
   override def process(implicit p: OpaqueData.ReifiedDataWrapper) = {
     CRUD.sendMagicLink(p.email("email")) recover {
